657243a41be6346fc05d3c40f364719b2ac4803c
Andrey N <grizzly-terror@yandex.ru>
2013-03-02 20:08:36 +0400

    Add a first version player

--- Juma/src/juma/window.java
+++ Juma/src/juma/window.java
@@ -5,6 +5,7 @@ package juma;
  *and the game loop. 
  */
 import org.lwjgl.LWJGLException;
+import org.lwjgl.input.Mouse;
  import org.lwjgl.opengl.Display; 
 import org.lwjgl.opengl.DisplayMode;
  import org.lwjgl.opengl.GL11;
@@ -54,25 +55,24 @@ public class window {
             e.printStackTrace();
             System.exit(0);
         }
-        Baground= Graphics.loadTexture("baground1");
+        Baground= Graphics.loadTexture("baground2");
 
         Shoot.CreateBall(0, 0);
         GL11.glMatrixMode(GL11.GL_PROJECTION);
         GL11.glLoadIdentity();
         GL11.glOrtho(0, Width, Height, 0, 1, -1);
         GL11.glMatrixMode(GL11.GL_MODELVIEW);   
-        GL11.glEnable(GL11.GL_DEPTH_TEST);
+        //GL11.glEnable(GL11.GL_DEPTH_TEST);
+        GL11.glDisable(GL11.GL_DEPTH_TEST);
         GL11.glEnable(GL11.GL_TEXTURE_2D);
-         GL11.glEnable(GL11.GL_ALPHA);
-        
+        GL11.glEnable(GL11.GL_ALPHA);
         GL11.glTexEnvi( GL11.GL_TEXTURE_ENV, GL11.GL_TEXTURE_ENV_MODE, GL11.GL_MODULATE );
-        
         drawBaground();
         printPlayer();
         while(!Display.isCloseRequested()){
              GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);   
             drawBaground();
-             //printPlayer();
+             printPlayer();
            /*  Key.IsBind();
              Shoot.Ball.run();
              Shoot.runNext();*/
@@ -90,7 +90,7 @@ public class window {
         Display.destroy();
     }
     void drawBaground(){
-  GL11.glDisable(GL11.GL_DEPTH_TEST);  
+ GL11.glDisable(GL11.GL_DEPTH_TEST);  
   GL11.glDepthMask(false);
 
   GL11.glLoadIdentity();
@@ -125,52 +125,45 @@ GL11.glEnd();
   GL11.glTexEnvf (GL11.GL_TEXTURE_ENV, GL11.GL_TEXTURE_ENV_MODE, GL11.GL_REPLACE); 
 
   GL11.glDepthMask(true);
-  GL11.glEnable(GL11.GL_DEPTH_TEST);  
+//  GL11.glEnable(GL11.GL_DEPTH_TEST);  
         
         
     }
     
       static void printPlayer(){
+         float Trot;
+        Trot = 90-(float)(Math.atan2(Mouse.getY()-Player.Y,Mouse.getX()-Player.X))*180/(float)Math.PI;
+        // Trot=0;
+           System.out.println(Trot);
         Player.TexPlayer.bind();
-       /* GL11.glEnable(GL11.GL_BLEND);
+        GL11.glEnable(GL11.GL_BLEND);
         GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
         GL11.glColor4ub((byte) 255, (byte) 255, (byte)255, (byte)255); 
-        GL11.glBegin(GL11.GL_QUADS);
-        GL11.glColor4f(1.0f,1.0f,1.0f,0.0f); 
-       */
-          GL11.glDisable(GL11.GL_DEPTH_TEST);  
-  GL11.glDepthMask(false);
+             // GL11.glLoadIdentity(); 
 
+        GL11.glMatrixMode(GL11.GL_MODELVIEW);
   GL11.glLoadIdentity();
-  GL11.glMatrixMode(GL11.GL_PROJECTION);
+  GL11.glDisable(GL11.GL_DEPTH_TEST);
+  GL11.glClear(GL11.GL_DEPTH_BUFFER_BIT );
   GL11.glPushMatrix();
-  GL11.glLoadIdentity();
-  GL11.glOrtho(0.0, 1.0, 0.0, 1.0, -1.0, 1.0);
- GL11.glTexEnvf (GL11.GL_TEXTURE_ENV, GL11.GL_TEXTURE_ENV_MODE, GL11.GL_MODULATE); 
+  GL11.glTranslatef(Player.X, Player.Y, 0);
+  GL11.glRotatef(Trot, 0, 0, 1);
 
-  GL11.glMatrixMode(GL11.GL_MODELVIEW);
-        
+        Player.TexPlayer.bind();
+        GL11.glBegin(GL11.GL_QUADS);
+        GL11.glColor4f(1.0f,1.0f,1.0f,0.0f);        
         GL11.glTexCoord2f(0,0);
-        GL11.glVertex2i(Player.X-(int)Player.Size/2,Player.Y-(int)Player.Size/2);
+        GL11.glVertex2i(-(int)Player.Size/2,-(int)Player.Size/2);
         GL11.glTexCoord2f(1,0);
-        GL11.glVertex2i(Player.X+(int)Player.Size/2,Player.Y-(int)Player.Size/2);
+        GL11.glVertex2i(+(int)Player.Size/2,-(int)Player.Size/2);
         GL11.glTexCoord2f(1,1);
-        GL11.glVertex2i(Player.X+(int)Player.Size/2,Player.Y+(int)Player.Size/2);
+        GL11.glVertex2i(+(int)Player.Size/2,+(int)Player.Size/2);
         GL11.glTexCoord2f(0,1);
-        GL11.glVertex2i(Player.X-(int)Player.Size/2,Player.Y+(int)Player.Size/2);
+        GL11.glVertex2i(-(int)Player.Size/2,+(int)Player.Size/2);
         GL11.glEnd();
-        //GL11.glDisable(GL11.GL_BLEND);
+        GL11.glDisable(GL11.GL_BLEND);
         
-  GL11.glMatrixMode(GL11.GL_PROJECTION);
-  GL11.glPopMatrix();
-  GL11.glMatrixMode( GL11.GL_MODELVIEW);
-  GL11.glPopMatrix();
-  GL11.glTexEnvf (GL11.GL_TEXTURE_ENV, GL11.GL_TEXTURE_ENV_MODE, GL11.GL_REPLACE); 
-
-  GL11.glDepthMask(true);
-  GL11.glEnable(GL11.GL_DEPTH_TEST);  
-        
-
+GL11.glPopMatrix();
     }
 }
 
